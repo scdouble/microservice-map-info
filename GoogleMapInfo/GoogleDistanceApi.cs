998f98fdffcdcf2edcc71424c8001a9b3dc3728b
Tatsu sei <doubledragontaz@gmail.com>
2022-03-24 22:57:44 +0900

    Complete google map distance api

--- GoogleMapInfo/GoogleDistanceApi.cs
+++ GoogleMapInfo/GoogleDistanceApi.cs
@@ -1,7 +1,31 @@
-﻿namespace GoogleMapInfo
+﻿using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace GoogleMapInfo
 {
     public class GoogleDistanceApi
     {
-
+        private readonly IConfiguration _configuration;
+     
+        public GoogleDistanceApi(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<GoogleDistanceData>
+            GetMapDistance(string originCity, string destinationCity)
+        {
+            var apiKey = _configuration["googleDistanceApi:apiKey"];
+            var googleDistanceApiUrl = _configuration["googleDistanceApi:apiUrl"];
+            googleDistanceApiUrl += $"units=imperial&origins={originCity}&destinations={destinationCity}&key={apiKey}";
+            using var client = new HttpClient();
+            var request = new
+                HttpRequestMessage(HttpMethod.Get, new Uri(googleDistanceApiUrl));
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            await using var data = await response.Content.ReadAsStreamAsync();
+            var distanceInfo = await
+                JsonSerializer.DeserializeAsync<GoogleDistanceData>(data);
+            return distanceInfo;
+        }
     }
 } 
