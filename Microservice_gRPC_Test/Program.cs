1278a5831c783a43fea2fcd16b78bacf3faba4ac
chen.a.sheng <chen.a.sheng@accenture.com>
2022-03-24 23:48:46 +0900

    add grpc test client

---
+++ Microservice_gRPC_Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Net.Client;
+
+
+var channel = GrpcChannel.ForAddress(new Uri("https://localhost:7033"));
+var client = new microservice_map_info.Protos.DistanceInfo.DistanceInfoClient(channel);
+var response = await client.GetDistanceAsync(
+    new microservice_map_info.Protos.Cities
+    {
+        OriginCity = "Topeka,KS",DestinationCity = "Los Angeles,CA"
+    });
+Console.WriteLine(response.Miles);
+Console.ReadKey();
