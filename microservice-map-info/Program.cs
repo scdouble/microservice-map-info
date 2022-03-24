04c4ec557c6dcd754eade06b183bdbb17f49e547
Tatsu sei <doubledragontaz@gmail.com>
2022-03-24 23:24:13 +0900

    Incorporating gRPC

--- microservice-map-info/Program.cs
+++ microservice-map-info/Program.cs
@@ -1,4 +1,5 @@
 using GoogleMapInfo;
+using microservice_map_info.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddGrpc();
 builder.Services.AddTransient<GoogleDistanceApi>();
 
 var app = builder.Build();
@@ -20,9 +22,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
-
-app.MapControllers();
+app.UseEndpoints(e =>
+{
+    e.MapGrpcService<DistanceInfoService>();
+    e.MapControllers();
+});
+// app.MapControllers();
 
 app.Run();
